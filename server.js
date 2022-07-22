a226d1a7bf918d342a4d3ba182d299bfa095d820
cchahinez <chahinez.work@gmail.com>
2022-07-22 01:52:26 +0100

    s

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { Client } = require("pg");
+
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+// postgresql-shallow-08047
+client.connect();
+
+client.query(
+    "SELECT table_schema,table_name FROM information_schema.tables;",
+    (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    }
+);
+
 app.get("/", (req, res) => {
     res.send("it works !!!");
 });
