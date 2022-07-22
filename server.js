8946f681511238846f1634f6ee4663f4a7db2a10
cchahinez <chahinez.work@gmail.com>
2022-07-22 15:07:12 +0100

    k

--- server.js
+++ server.js
@@ -13,58 +13,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //var DATABASE_URL = "postgresql-shallow-08047";
 
+const client = new Client({
+    user: "ybvvfosddpptkx",
+    host: "ec2-54-161-255-125.compute-1.amazonaws.com",
+    database: "d38o6h85l1i369",
+    password:
+        "1d5e26091ee3fcec785f53656a10c96f08c559ba8d847c2c95abde4057ffbcf4",
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+client.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 app.get("/", (req, res) => {});
-app.post("/saving", async (req, res) => {
-    const client = new Client({
-        user: "ybvvfosddpptkx",
-        host: "ec2-54-161-255-125.compute-1.amazonaws.com",
-        database: "d38o6h85l1i369",
-        password:
-            "1d5e26091ee3fcec785f53656a10c96f08c559ba8d847c2c95abde4057ffbcf4",
-        port: 5432,
-        ssl: {
-            rejectUnauthorized: false,
-        },
-    });
+
+/*app.post("/saving", async (req, res) => {
     const data = req.body.nom;
     var queryModel = `INSERT INTO artists VALUES(159,'o')`;
 
     // postgresql-shallow-08047
-    await client.connect(function (err) {
-        if (err) throw err;
-        console.log("Connected!");
-    });
 
     await client.query(queryModel, (err, res) => {
         if (err) throw err;
         client.end();
     });
-    /*
-    const insertUser = async (userName) => {
-        try {
-            await client.connect(); // gets connection
-            await client.query(
-                `INSERT INTO "artists" ("name")  
-             VALUES ($1)`,
-                [userName]
-            ); // sends queries
-            return true;
-        } catch (error) {
-            console.error(error.stack);
-            return false;
-        } finally {
-            await client.end(); // closes connection
-        }
-    };
-
-    insertUser(data).then((result) => {
-        if (result) {
-            console.log("User inserted");
-        }
-    });*/
-
     res.redirect("/");
-});
+});*/
 
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
