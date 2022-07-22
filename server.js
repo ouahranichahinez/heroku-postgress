af5ad9152be2f8309d4836ecabb4f3601a7caf56
cchahinez <chahinez.work@gmail.com>
2022-07-22 13:24:30 +0100

    j

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.post("/saving", async (req, res) => {
         },
     });
     const data = req.body.nom;
-    var queryModel = `INSERT INTO artists(id,name) VALUES('3333','${data}')`;
+    var queryModel = `INSERT INTO artists(id,name) VALUES('003','${data}')`;
 
     // postgresql-shallow-08047
     client.connect(function (err) {
