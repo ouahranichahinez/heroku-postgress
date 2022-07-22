8bc98ce206ce0b693528ddad08bf3b71acabd76e
cchahinez <chahinez.work@gmail.com>
2022-07-22 12:34:25 +0100

    l

--- server.js
+++ server.js
@@ -3,31 +3,35 @@ import { Client } from "pg";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// use the express-static middleware
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
 //var DATABASE_URL = "postgresql-shallow-08047";
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false,
-    },
-});
-// postgresql-shallow-08047
-client.connect();
+app.post("/saving", async (req, res) => {
+    const client = new Client({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
+    // postgresql-shallow-08047
+    client.connect();
 
-client.query(
-    "SELECT table_schema,table_name FROM information_schema.tables;",
-    (err, res) => {
-        if (err) throw err;
-        for (let row of res.rows) {
-            console.log(JSON.stringify(row));
+    client.query(
+        `insert into artists(name) values(${req.body.nom});`,
+        (err, res) => {
+            if (err) throw err;
+            for (let row of res.rows) {
+                console.log(JSON.stringify(row));
+            }
+            client.end();
         }
-        client.end();
-    }
-);
-
-app.get("/", (req, res) => {
-    res.send("it works !!!");
+    );
 });
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
