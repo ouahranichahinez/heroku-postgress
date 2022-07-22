51d96368d7c5d9682fb54691aa446fa00d415427
cchahinez <chahinez.work@gmail.com>
2022-07-22 01:21:24 +0100

    l

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 
+app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.send("hello there !");
 });
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
